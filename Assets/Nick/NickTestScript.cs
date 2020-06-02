6dc7d19383c8ff3dfba96aecbd37aa4bf0578822
Nicholas Burke <nicholas.arthur.burke@gmail.com>
2020-06-02 14:23:19 +1000

    Nick Folder and Temps Added

---
+++ Assets/Nick/NickTestScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.EventSystems;
+
+public class NickTestScript : MonoBehaviour
+{
+    public Transform myHand;
+    public GameObject mePiece;
+    public Rigidbody piece;
+    private bool holdingPiece = false;
+
+    void Start()
+    {
+        piece = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    public void pieceInteract()
+    {
+        if (holdingPiece == false)
+        {
+            piece.transform.parent = myHand.transform;
+            holdingPiece = true;
+        }
+       else if (holdingPiece == true)
+        {
+            piece.transform.parent = null;
+            holdingPiece = false;
+        }
+    }
+  
+ /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Respawn")
+        {
+            piece.useGravity = false;
+        }
+        else
+        {
+            piece.useGravity = true;
+        }
+    }
+    */
+}
