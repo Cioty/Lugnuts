716302a62803ee9ac62f5775be7c119a3dc1b9e7
Nicholas Burke <nicholas.arthur.burke@gmail.com>
2020-06-07 23:16:38 +1000

    Further Drag/Drop from Nick

--- Assets/Nick/NickTestScript.cs
+++ Assets/Nick/NickTestScript.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VR;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class NickTestScript : MonoBehaviour
 {
     public Transform myHand;
+    public Transform player;
     public GameObject mePiece;
     public Rigidbody piece;
     private bool holdingPiece = false;
+    public bool lookedAt = false;
+    public SphereCollider entrancePeg;
+    public int pieceValue;
+    public Text pieceDisplay; 
 
     void Start()
     {
@@ -18,29 +24,47 @@ public class NickTestScript : MonoBehaviour
 
     public void pieceInteract()
     {
-        if (holdingPiece == false)
+        if (holdingPiece == false && this.gameObject.tag == "Respawn")
         {
             piece.transform.parent = myHand.transform;
             holdingPiece = true;
+            piece.isKinematic = true;
+           mePiece.transform.rotation = Quaternion.LookRotation(myHand.position);
+
         }
-       else if (holdingPiece == true)
+        else if (holdingPiece == true)
         {
             piece.transform.parent = null;
+            piece.isKinematic = false;
             holdingPiece = false;
+
         }
     }
-  
- /*
-    private void OnTriggerEnter(Collider other)
+
+    public void ActiveLook()
     {
-        if (other.tag == "Respawn")
+        this.gameObject.tag = "Respawn";
+    }
+    public void DeActiveLook()
+    {
+        this.gameObject.tag = "Untagged";
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Finish" && holdingPiece == false)
         {
+            piece.isKinematic = true;
             piece.useGravity = false;
+            pieceDisplay.text = pieceValue.ToString();
         }
-        else
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Finish" && holdingPiece == true)
         {
             piece.useGravity = true;
+            pieceDisplay.text = "0";
         }
     }
-    */
 }
