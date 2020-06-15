d88b323629665755b1dda75a32e95eb8b297faeb
Nicholas Burke <nicholas.arthur.burke@gmail.com>
2020-06-15 14:14:52 +1000

    15/06 Nick Push

--- Assets/Nick/ArmChecker.cs
+++ Assets/Nick/ArmChecker.cs
@@ -6,39 +6,36 @@ public class ArmChecker : MonoBehaviour
 {
 
     private NickTestScript parentScript;
-    public Transform connectionPosition;
-    //public bool connectingSocket;
 
-    //public int connectionNumber;
+    //the position where other pieces move to
+    public Transform connectionPosition;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        parentScript = GetComponentInParent<NickTestScript>();
-        //connectionPosition = parentScript.connectionArea;
-        //connectionPosition = parentScript.connectionPoints[connectionNumber].transform;
-
-        //if (connectingSocket == false)
-        //{
-        //    connectionPosition = null;
-        //}
+        parentScript = GetComponentInParent<NickTestScript>();  
     }
 
+   
     public void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Finish" && parentScript.holdingPiece == false /*&& connectingSocket == false*/)
+        //Checks if the player is not holding the object and the tag of the entered collier
+        if (other.tag == "Finish" && parentScript.holdingPiece == false /*&& other.gameObject.GetComponent<NickTestScript>().connected == false*/)
         {
-            parentScript.piece.isKinematic = true;
-            parentScript.piece.useGravity = false;
-            parentScript.piece.rotation = other.gameObject.GetComponent<ArmChecker>().connectionPosition.transform.rotation;
-            parentScript.piece.position = other.gameObject.GetComponent<ArmChecker>().connectionPosition.transform.position;
+            parentScript.pieceRigidBody.isKinematic = true;
+            parentScript.pieceRigidBody.useGravity = false;
+            parentScript.pieceRigidBody.rotation = other.gameObject.GetComponent<ArmChecker>().connectionPosition.transform.rotation;
+            parentScript.pieceRigidBody.position = other.gameObject.GetComponent<ArmChecker>().connectionPosition.transform.position;
+           // parentScript.connected = true;
         }
     }
     public void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Finish" /*&& parentScript.holdingPiece == true*/)
+        if (other.tag == "Finish")
         {
-            parentScript.piece.useGravity = true;
+            parentScript.pieceRigidBody.useGravity = true;
+           // parentScript.connected = false;
         }
     }
 }
--- Assets/Nick/NickTestScript.cs
+++ Assets/Nick/NickTestScript.cs
@@ -7,53 +7,75 @@ using UnityEngine.UI;
 
 public class NickTestScript : MonoBehaviour
 {
+    //Transform of the reticle/player gaze
     public Transform myHand;
+
+    //Player's transform
     public Transform player;
-    public GameObject mePiece;
-    public Rigidbody piece;
+
+    //The piece's gameobject
+    public GameObject thisGameObject;
+
+    //The rigidbody on the piece
+    public Rigidbody pieceRigidBody;
+
+    //bool that checks if the player is holding a piece
     [HideInInspector]
     public bool holdingPiece = false;
-    public bool lookedAt = false;
+
+    //bool that checks if a piece is connected to another piece
+    [HideInInspector]
+    public bool connected = false;
+
+    //Materials need to be changed to shaders later
+    //Materials/Shaders that change if player is looking at object
     public Material hoverMaterial;
     public Material baseMaterial;
-    //public Transform connectionArea;
-    //public GameObject[] connectionPoints;
+   
 
     void Start()
     {
-        piece = this.gameObject.GetComponent<Rigidbody>();
-        /*
-        for(int i = 0; i <= connectionPoints.Length; i++)
-        {
-            connectionPoints[i].GetComponent<ArmChecker>().connectionNumber = i;
-        }
-        */
+        //finds the piece's rigidbody
+        pieceRigidBody = this.gameObject.GetComponent<Rigidbody>();
     }
 
+
+    //occurs when reticle is pointing over object and player presses button
     public void pieceInteract()
     {
+        //tag names need to be changed
+
+        //if player is not holding piece and the piece is being looked at
         if (holdingPiece == false && this.gameObject.tag == "Respawn")
         {
-            piece.transform.parent = myHand.transform;
+            this.gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+            //parents the hand to the object so that it follows where the player looks
+            pieceRigidBody.transform.parent = myHand.transform;
             holdingPiece = true;
-            piece.isKinematic = true;
-            mePiece.transform.rotation = Quaternion.LookRotation(myHand.position);
+            pieceRigidBody.isKinematic = true;
+            //changes the position and rotation of the object to be the same distance/rotation each time
+            this.gameObject.transform.position = myHand.transform.position;
 
         }
+        //drops the piece if it's being held, and it becomes subject to gravity
         else if (holdingPiece == true)
         {
-            piece.transform.parent = null;
-            piece.isKinematic = false;
-            holdingPiece = false;
-            
+            pieceRigidBody.transform.parent = null;
+            pieceRigidBody.isKinematic = false;
+            holdingPiece = false;   
         }
     }
 
+
+    //sets game object currently being looked at
+    //occurs when player looks at object
     public void ActiveLook()
     {
+        //tag names need to be changed
         this.gameObject.tag = "Respawn";
         this.gameObject.GetComponent<Renderer>().material.color = hoverMaterial.color;
     }
+    //occurs when player stops looking at object
     public void DeActiveLook()
     {
         this.gameObject.tag = "Untagged";
