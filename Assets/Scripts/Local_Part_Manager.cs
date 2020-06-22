f9546d20a359a74b220549b88f91ec326e60fa37
Matt Brophy <gumhewn@gmail.com>
2020-06-22 14:03:38 +1000

    Monday midday push

--- Assets/Scripts/Local_Part_Manager.cs
+++ Assets/Scripts/Local_Part_Manager.cs
@@ -23,6 +23,22 @@ public class Local_Part_Manager : MonoBehaviour
     [HideInInspector]
     public Node thisNode;
 
+    //Glowfade // All these values are in seconds.
+    public float colorAttackTime = 0.2f;
+    public float colorSustainTime = 0.3f;
+    public float silhouetteAttackTime = 0.2f;
+
+    //Glowfade shader properties
+    public float colorMeter;
+    public float shadowMeter;
+
+    private void Awake()
+    {
+        var materialRef = gameObject.transform.GetChild(0).GetComponent<Renderer>().material;
+        //colorMeter = materialRef.colorMeter;
+        //shadowMeter = materialRef.shadowMeter;
+    }
+
     public void AddPieceToGlobalManager(GameObject socketObject)
     {
         globalPartManager.AddPlayerPart(shape, color, socketObject, gameObject);
@@ -31,5 +47,28 @@ public class Local_Part_Manager : MonoBehaviour
     public void RemovePieceFromGlobalManager()
     {
         globalPartManager.RemovePlayerPart(gameObject);
+    }
+
+    public IEnumerator GlowFade()
+    {
+        //color increases every frame
+        for (float i = 0; i < colorAttackTime; i += Time.deltaTime)
+        {
+            colorMeter = i / colorAttackTime;
+            yield return null;
+        }
+
+        //color sustain
+        colorMeter = 1;
+        yield return new WaitForSeconds(colorSustainTime);
+
+        for (float i = 0; i < silhouetteAttackTime; i += Time.deltaTime)
+        {
+            shadowMeter = i / silhouetteAttackTime;
+            yield return null;
+        }
+
+        shadowMeter = 1;
+
     }
 }
