d52622cda4564514dce26cf464af2d52615e7bbb
Matt Brophy <gumhewn@gmail.com>
2020-06-24 14:00:57 +1000

    commit for nick. Small changes to scene

---
+++ Assets/Scripts/Crate_Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate_Manager : MonoBehaviour
+{
+    public GameObject crate1;
+    public GameObject crate2;
+    private Animator crate1Animator;
+    private Animator crate2Animator;
+    private bool crate1Bool;
+    private bool crate2Bool;
+
+    private void Awake()
+    {
+        crate1Animator = crate1.GetComponent<Animator>();
+        crate2Animator = crate2.GetComponent<Animator>();
+        crateSet();
+    }
+
+    private void CrateSet()
+    {
+        crate1Animator.SetBool("Entry", true);
+        crate2Animator.SetBool("Entry", false);
+    }
+
+    public void CrateCycle()
+    {
+        crate1Animator.Set
+    }
+}
---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    private Animator thisAnimator;
+
+    private void Awake()
+    {
+        thisAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    public void LeverStop()
+    {
+        thisAnimator.SetBool("pull", false);
+    }
+}
